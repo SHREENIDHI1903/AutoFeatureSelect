25b29beea9981e89c67178bb729786f4da7a9618
unknown <shreenidhi1903@gmail.com>
2025-08-08 00:07:25 +0530

    published

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
 from setuptools import setup, find_packages
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
-    name='autofeatureselect',
-    version='0.1.0',
-    author='Shreenidhi T H',
-    author_email='xyz@gmail.com',
-    description='A simple feature selection package for ML',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/autofeatureselect',
+    name="autofeatureselect",
+    version="0.1.4",
+    author="Shreenidhi T H",
+    author_email="your.email@example.com",
+    description="A Python package for automated feature selection in ML workflows.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/autofeatureselect",
     packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'numpy',
-        'scikit-learn',
-        'matplotlib',
-        'shap'
-    ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "xgboost",
+        "shap",
+        "statsmodels"
+
+    ],
 )
